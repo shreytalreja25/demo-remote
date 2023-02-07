c276ee648dbc23eb545bb3bf6e8c8493c911a38f
Shrey Talreja <shreytalreja25@gmail.com>
2023-02-07 11:10:54 +0530

    Second Update to App in update branch

--- App.js
+++ App.js
@@ -1,1 +1,12 @@
 console.log("Hello World")
+
+function World(Name) {
+    var Name = prompt("Enter Name");
+    if (Name === "Shrey") {
+        console.log("Access Granted to " + Name)
+    } else {
+        console.log("Access Denied to " + Name)
+    }
+}
+
+World();
